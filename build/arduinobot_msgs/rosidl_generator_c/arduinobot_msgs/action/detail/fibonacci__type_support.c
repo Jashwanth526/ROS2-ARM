33e9709634f3e6168bd09832bef30eedfcd2c1ef
Jashwanth K <kjashwanthk@gmail.com>
2025-11-04 11:23:27 +0530

    Replace RGB camera with depth camera and adjust position/tilt

--- build/arduinobot_msgs/rosidl_generator_c/arduinobot_msgs/action/detail/fibonacci__type_support.c
+++ build/arduinobot_msgs/rosidl_generator_c/arduinobot_msgs/action/detail/fibonacci__type_support.c
@@ -4,10 +4,10 @@
 
 #include <string.h>
 
-#include "arduinobot_msgs/action/detail/fibonacci__type_support.h"
 #include "rosidl_typesupport_interface/macros.h"
-#include "arduinobot_msgs/action/detail/fibonacci__struct.h"
+#include "arduinobot_msgs/action/detail/fibonacci__type_support.h"
 #include "arduinobot_msgs/action/detail/fibonacci__functions.h"
+#include "arduinobot_msgs/action/detail/fibonacci__struct.h"
 
 #ifdef __cplusplus
 extern "C"
